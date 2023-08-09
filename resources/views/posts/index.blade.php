5b3350912abc4f50a014fb71b21e5c8d0b81c3e6
Rufus8702 <Jamaludinahmad013@gmail.com>
2023-08-09 10:38:52 +0700

    first laravel

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Data Posts - SantriKoding.com</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+</head>
+<body style="background: lightgray">
+    @forelse ($posts as $post)
+        <div class = "data">
+            <p>$post</p>
+        </div>
+    @empty
+        <div>
+            <p>data belum tersedia</p>
+        </div>
+    @endforelse
+</body>
+</html>
