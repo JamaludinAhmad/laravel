c1543f6df4c95ab9cde7b0c6b78723d7c6cae4f5
Rufus8702 <Jamaludinahmad013@gmail.com>
2023-08-10 14:57:17 +0700

    memakai paginate

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -63,6 +63,8 @@
         </tbody>
       </table>
 
+      {{$employees -> links()}}
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   </body>
 </html>
